7a77b7758b4582fd57d6814ce05954c9ec95b493
sa.bhatia <saurabh96bhatia@gmail.com>
2019-10-09 11:40:05 +0530

    Added Get and Post Method Checking

--- tests/Api_Test.py
+++ tests/Api_Test.py
@@ -11,34 +11,52 @@ Add assert_code param to the input to compare its return code
 
 
 '''
+
+
 class APITest():
     def __init__(self, input):
 
-        #Converting  input dict to Class attributes
+        # Converting  input dict to Class attributes
         self.__dict__ = input
 
-    def start_test(self):
-        print ("Starting Testing  for {}".format(self.base_url + self.endpoint))
+    def get_request(self, url ):
 
-        response = requests.get(self.base_url + self.endpoint)
+        return requests.get(url)
+
+    def post_request(self , url , body):
+        return requests.post(url , body = body)
+
+
+    def start_test(self):
+        print("Starting Testing  for {}".format(self.base_url + self.endpoint))
+
+        #Depending on the method in the configuration it will either post a request or get the request
+
+        if self.method == 'get':
+            response = self.get_request(self.base_url + self.endpoint)
+
+        if self.method == 'post':
+            response = self.post_request(self.base_url + self.endpoint)
+
+        #response = requests.get(self.base_url + self.endpoint)
 
         if hasattr(self, 'assert_code'):
-            print ("Testing of Status Code")
+            print("Testing of Status Code")
             rsp = response.status_code
 
             if self.assert_code == str(rsp):
-                print ("Tescase Passed")
+                print("Tescase Passed")
 
             else:
-                print ("Testcase Failed")
+                print("Testcase Failed")
 
-        if hasattr(self , 'assert_res'):
+        if hasattr(self, 'assert_res'):
             '''
             If some passed to assert response also then we can implement this 
             '''
             pass
 
-        if hasattr(self , 'assert_schema'):
+        if hasattr(self, 'assert_schema'):
             '''
             if some passed to assert the schema then we can implement this 
             '''
@@ -46,23 +64,24 @@ class APITest():
 
 
 
+
 if __name__ == "__main__":
 
-    input = [ {
-        'base_url' :'http://dummy.restapiexample.com/api/v1/',
-        'endpoint' : 'employee/81988' ,
+    input = [{
+        'base_url': 'http://dummy.restapiexample.com/api/v1/',
+        'endpoint': 'employee/81988',
+        'method' : 'get',
         'type': 'json',
-        'assert_key' : 'employee_salary',
+        'assert_key': 'employee_salary',
         'assert_code': "406",
-
 
     },
         {
             'base_url': 'http://dummy.restapiexample.com/api/v1/',
             'endpoint': 'employees/212',
             'type': 'json',
+            'method' : 'get',
             'assert_code': "406",
-
 
         },
 
@@ -72,7 +91,22 @@ if __name__ == "__main__":
             'type': 'json',
             'assert_code': "406",
 
+        },
+
+        {
+            'base_url' : 'http://localhost:8080',
+            'endpoint' : '/post',
+            'method' : 'post',
+            'body' : '{"key":"test" , "value" : "2"}',
+            'assert_code' : '200'
+        },
+        {
+            'base_url': 'http://localhost:8080',
+            'endpoint': '/get/test',
+            'method': 'get',
+            'assert_code': '200'
         }
+
     ]
 
     for each in input:
